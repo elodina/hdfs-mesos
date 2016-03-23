c7dc3b3ed2ccd28a96da66a5439f93164089d340
yohan <dmitry.pekar@gmail.com>
2016-03-23 18:22:19 +0200

    misc

--- util-mesos/src/java/net/elodina/mesos/test/MesosTestCase.java
+++ util-mesos/src/java/net/elodina/mesos/test/MesosTestCase.java
@@ -33,7 +33,8 @@ public class MesosTestCase {
     }
 
     public Offer offer() { return offer("cpus:1;mem:1024;ports:0..100"); }
-    public Offer offer(String resources) { return offer("" + UUID.randomUUID(), "" + UUID.randomUUID(), "" + UUID.randomUUID(), "host", resources, null); }
+    public Offer offer(String resources) { return offer("host", resources); }
+    public Offer offer(String hostname, String resources) { return offer("" + UUID.randomUUID(), "" + UUID.randomUUID(), "" + UUID.randomUUID(), hostname, resources, null); }
     public Offer offer(String id, String frameworkId, String slaveId, String hostname, String resources, String attributes) {
         Offer.Builder builder = Offer.newBuilder()
             .setId(OfferID.newBuilder().setValue(id))

4d807e8962c28ef4faad5c7d7b043501e6c40f55
yohan <dmitry.pekar@gmail.com>
2016-04-05 15:16:35 +0300

    added framework-role and framework-timeout params

--- src/java/net/elodina/mesos/hdfs/SchedulerCli.java
+++ src/java/net/elodina/mesos/hdfs/SchedulerCli.java
@@ -3,6 +3,7 @@ package net.elodina.mesos.hdfs;
 import joptsimple.OptionException;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import net.elodina.mesos.util.Period;
 
 import java.io.*;
 import java.util.HashMap;
@@ -15,14 +16,18 @@ import static net.elodina.mesos.hdfs.Cli.*;
 
 public class SchedulerCli {
     public static void handle(List<String> args, boolean help) {
+        Scheduler.Config config = Scheduler.$.config;
+
         OptionParser parser = new OptionParser();
         parser.accepts("api", "Binding host:port for http/artifact server.").withRequiredArg().ofType(String.class);
-        parser.accepts("storage", " Storage for cluster state.\nDefault - file:hdfs-mesos.json.\nExamples:\n  file:hdfs-mesos.json;\n  zk:master:2181/hdfs-mesos;\n  zk:m1:2181,m2:2181/hdfs-mesos;").withRequiredArg().ofType(String.class);
+        parser.accepts("storage", " Storage for cluster state.\nDefault - " + config.storage + ".\nExamples:\n  file:hdfs-mesos.json;\n  zk:master:2181/hdfs-mesos;\n  zk:m1:2181,m2:2181/hdfs-mesos;").withRequiredArg().ofType(String.class);
 
         parser.accepts("master", "Mesos Master addresses.").withRequiredArg().ofType(String.class);
         parser.accepts("user", "Mesos user. Default - none").withRequiredArg().ofType(String.class);
 
-        parser.accepts("framework-name", "Framework name. Defaults to hdfs.").withRequiredArg().ofType(String.class);
+        parser.accepts("framework-name", "Framework name. Default - " + config.frameworkName + ".").withRequiredArg().ofType(String.class);
+        parser.accepts("framework-role", "Framework role. Default- " + config.frameworkRole + ".").withRequiredArg().ofType(String.class);
+        parser.accepts("framework-timeout", "Framework failover timeout. Default - " + config.frameworkTimeout + ".").withRequiredArg().ofType(String.class);
 
         if (help) {
             printLine("Generic Options");
@@ -65,7 +70,15 @@ public class SchedulerCli {
         String frameworkName = (String) options.valueOf("framework-name");
         if (frameworkName == null) frameworkName = defaults.get("framework-name");
 
-        Scheduler.Config config = Scheduler.$.config;
+        String frameworkRole = (String) options.valueOf("framework-role");
+        if (frameworkRole == null) frameworkRole = defaults.get("framework-role");
+
+        String frameworkTimeout = (String) options.valueOf("framework-timeout");
+        if (frameworkTimeout == null) frameworkTimeout = defaults.get("framework-timeout");
+        if (frameworkTimeout != null)
+            try { new Period(frameworkTimeout); }
+            catch (IllegalArgumentException e) { throw new Error("invalid framework-timeout"); }
+
         config.api = api;
         if (storage != null) config.storage = storage;
 
@@ -73,6 +86,9 @@ public class SchedulerCli {
         config.user = user;
 
         if (frameworkName != null) config.frameworkName = frameworkName;
+        if (frameworkRole != null) config.frameworkRole = frameworkRole;
+        if (frameworkTimeout != null) config.frameworkTimeout = new Period(frameworkTimeout);
+
         Scheduler.$.run();
     }
 
